c4aa9e132fbd45e07fa435b03d172560e6532460
alexdlb <info@dlbinformatica.com>
2026-04-13 17:24:47 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // rebuild trigger
-import { defineConfig } from "vite";
-
 export default defineConfig({
   plugins: [react()],
   server: {
