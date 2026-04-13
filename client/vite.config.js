93db867d3314f35eb4fee44e36953081d16ccbee
alexdlb <info@dlbinformatica.com>
2026-04-13 17:11:41 +0200

    force frontend rebuild

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// rebuild trigger
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
   server: {
